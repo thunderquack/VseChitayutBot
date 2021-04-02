6ef27567ca04c64ada5cdcddfca403012318bb8c
Egor Ivanov <thunderquack@mail.ru>
2021-04-02 15:51:40 +0200

    Add project files.

---
+++ VseChitayutBot/Bot.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+using Telegram.Bot;
+using Telegram.Bot.Args;
+
+namespace VseChitayutBot
+{
+    internal class Bot : TelegramBotClient
+    {
+        public List<int> admins = new List<int>();
+        public bool Exit { get; private set; } = false;
+        public Bot(string token, HttpClient httpClient = null) : base(token, httpClient)
+        {
+            OnMessage += ProcessMessage;
+        }
+
+        private void ProcessMessage(object sender, MessageEventArgs e)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ VseChitayutBot/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using System.Threading;
+
+namespace VseChitayutBot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string apiKey = File.ReadAllText("apikey.txt");
+            Bot bot = new Bot(apiKey);
+            bot.StartReceiving();
+            while (!bot.Exit)
+            {
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
