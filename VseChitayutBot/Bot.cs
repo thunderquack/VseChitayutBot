37002cbef9420aadc4ed9bb4bd23a939ea592e98
Egor Ivanov <thunderquack@mail.ru>
2021-04-02 16:32:04 +0200

    settings

--- VseChitayutBot/Bot.cs
+++ VseChitayutBot/Bot.cs
@@ -1,22 +1,77 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using Telegram.Bot;
 using Telegram.Bot.Args;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
 
 namespace VseChitayutBot
 {
     internal class Bot : TelegramBotClient
     {
-        public List<int> admins = new List<int>();
+        public const string SETTINGSFILE = "settings.txt";
+        public List<int> admins = new List<int>()
+        {
+            98071669, // Egor
+            577400792 // Inna
+        };
         public bool Exit { get; private set; } = false;
+        private long channelId;
+        private long chatId;
+        private bool productiveMode = false;
         public Bot(string token, HttpClient httpClient = null) : base(token, httpClient)
         {
+            LoadSettings();
             OnMessage += ProcessMessage;
         }
 
         private void ProcessMessage(object sender, MessageEventArgs e)
         {
-            throw new System.NotImplementedException();
+            if (admins.Contains(e.Message.From.Id) && e.Message.ForwardFromChat != null)
+            {
+                SetChannelSettings(e.Message);
+            }
+        }
+
+        private void SetChannelSettings(Message message)
+        {
+            long id = message.ForwardFromChat.Id;
+            Chat chat = GetChatAsync(id).Result;
+            if (chat.Type == Telegram.Bot.Types.Enums.ChatType.Channel)
+                channelId = id;
+            if (chat.Type == ChatType.Group||chat.Type==ChatType.Supergroup)
+                chatId = id;
+            if (channelId != 0 && chatId != 0) SaveSettings();
+        }
+        private void LoadSettings()
+        {
+            if (System.IO.File.Exists(SETTINGSFILE))
+            {
+                try
+                {
+                    string[] lines = System.IO.File.ReadAllLines(SETTINGSFILE);
+                    channelId = Convert.ToInt64(lines[0]);
+                    chatId = Convert.ToInt64(lines[1]);
+                    productiveMode = true;
+                }
+                catch (Exception err)
+                {
+                    productiveMode = false;
+                }
+            }
+            else
+            {
+                productiveMode = false;
+            }
+        }
+        private void SaveSettings()
+        {
+            System.IO.File.WriteAllLines(SETTINGSFILE, new string[]
+                 {
+                     Convert.ToString(channelId),
+                     Convert.ToString(chatId)
+                 });
         }
     }
 }
